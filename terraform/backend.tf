7fa9e584646bc83b7b0d18c0287eafe4982c4639
MikeKravchenko <kravchenko.soft@gmail.com>
2025-07-08 22:29:01 -0400

    workspace2

---
+++ terraform/backend.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "behavox-platform-sre-tfstate"
+  force_destroy = true
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tfstate_lock" {
+  name         = "behavox-platform-sre-tfstate-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock Table"
+  }
+}
